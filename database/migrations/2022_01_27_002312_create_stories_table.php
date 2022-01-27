a1f3a619a4ecd7cc4702179c59d4c52a94081e50
juankOrtiz <juank.ortiz.1991@gmail.com>
2022-01-26 22:00:08 -0300

    Create model, migration and controller for Issue, Story and Title

---
+++ database/migrations/2022_01_27_002312_create_stories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("issue_id");
+            $table->string("name");
+            $table->integer("order")->nullable();
+            $table->timestamps();
+
+            $table->foreign("issue_id")->references("id")->on("issues");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
